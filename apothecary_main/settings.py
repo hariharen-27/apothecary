69e27abe3f3d2a7d86c598394e7363b166dfc6c0
hariharen-27 <hariharen27302@gmail.com>
2023-05-13 12:30:32 +0530

    forgot features

--- apothecary_main/settings.py
+++ apothecary_main/settings.py
@@ -144,3 +144,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# email configuration
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'apothecaryofficial27@gmail.com'
+EMAIL_HOST_PASSWORD = 'ncdfcmcwuriiqkjp'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'apothecary Marketplace <apothecaryofficial27@gmail.com>'
